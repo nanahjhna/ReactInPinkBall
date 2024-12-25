969e768192915e399ac9e9e2ad7a85d7eab55aa9
nanahjhna <nanahjhna7@naver.com>
2024-12-26 04:49:50 +0900

    App.js 수정

--- src/pages/Home/App.js
+++ src/pages/Home/App.js
@@ -1,12 +1,14 @@
 // Home.js
 import '../../styles/App.css'; // 올바른 상대 경로로 수정
+import logo from '../../logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. HAN
         </p>
         <a
           className="App-link"
